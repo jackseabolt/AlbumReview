8e18f77abe6bdf62b67404d400b1271b20d4a356
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-01-19 14:57:49 -0800

    style changes

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,6 +2,9 @@ class Album < ApplicationRecord
 	has_many :reviews
 	belongs_to :user
 	validates :title, presence: true
+	validates :cover, presence: true
+	validates :year, presence: true
+	validates :artist, presence: true
 	
 	has_attached_file :cover, styles: { normal: "400x400#", medium: "400x400>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", default_style: :normal
     validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/
